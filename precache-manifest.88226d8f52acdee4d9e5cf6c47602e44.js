e2a42a578ba582e3de7121d38a7185e54397fb28
Sagar Jadhav <sagarjadhav27@gmail.com>
2022-10-05 15:46:16 +0530

    Updates

---
+++ precache-manifest.88226d8f52acdee4d9e5cf6c47602e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ae99016b7714105c3ae",
+    "url": "/monsters-rolodex/static/css/main.6e9756e3.chunk.css"
+  },
+  {
+    "revision": "5ae99016b7714105c3ae",
+    "url": "/monsters-rolodex/static/js/main.5ae99016.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/monsters-rolodex/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "274396811fff8c54c05e",
+    "url": "/monsters-rolodex/static/js/2.27439681.chunk.js"
+  },
+  {
+    "revision": "02c1b23ffcb7e58431d5",
+    "url": "/monsters-rolodex/static/js/runtime~main.02c1b23f.js"
+  },
+  {
+    "revision": "72de84c0b0ded0a5f7aef6ab95de96ce",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
